323b12c97d0efdaa4e69e8c2f1df488bae5e4816
apmorr <144373529+apmorr@users.noreply.github.com>
2023-12-10 21:19:25 -0500

    Create TestMyBSTMap.py

---
+++ TestMyBSTMap.py
@@ -0,0 +1,19 @@
+import unittest, random
+from MyBSTMap import MyBSTMap
+
+
+class TestMyBSTMap(unittest.TestCase):
+    def test_equal_empty(self):
+        """ADD DOCSTRING"""
+
+    def test_equal_multiplenodes(self):
+        """ADD DOCSTRING"""
+
+    def test_notequal_multiplenodes_difshapes(self):
+        """ADD DOCSTRING"""
+
+    def test_notequal_multiplenodes_difkvs(self):
+        """ADD DOCSTRING"""
+
+
+unittest.main()
